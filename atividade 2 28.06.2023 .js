5ebaba71761a8ceec55c80e4e866a2121c05666f
KemyssonFernando <130847061+KemyssonFernando@users.noreply.github.com>
2023-06-28 14:31:38 -0300

    atividade 2 28/06/2023
    atividade 2 feita com sucesso !!!

---
+++ atividade 2 28.06.2023 .js
@@ -0,0 +1,13 @@
+let numero = new Number(200);
+console.log(numero);
+
+console.log(numero.toString())
+
+let nao_eh_numero = Number.isNaN("kemysson"); 
+console.log(nao_eh_numero)
+let eh_numero =Number.isNaN(705752);
+console.log(eh_numero)
+
+let x = 3000
+console.log ('o valor da propriedade epilson é:' +number.epilson);
+console.log (x< number.epilson);
